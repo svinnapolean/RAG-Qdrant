6ff07f5d642ab6aeb16f82d30aeb6d7a79205ce1
Vincent Susai <svinnapolean@gmail.com>
2025-08-16 22:22:37 -0400

    update project1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+List<CloudService> cloudServices =
+[
+    new() {
+            Key = 0,
+            Name = "Azure App Service",
+            Description = "Host .NET, Java, Node.js, and Python web applications and APIs in a fully managed Azure service. You only need to deploy your code to Azure. Azure takes care of all the infrastructure management like high availability, load balancing, and autoscaling."
+    },
+    new() {
+            Key = 1,
+            Name = "Azure Service Bus",
+            Description = "A fully managed enterprise message broker supporting both point to point and publish-subscribe integrations. It's ideal for building decoupled applications, queue-based load leveling, or facilitating communication between microservices."
+    },
+    new() {
+            Key = 2,
+            Name = "Azure Blob Storage",
+            Description = "Azure Blob Storage allows your applications to store and retrieve files in the cloud. Azure Storage is highly scalable to store massive amounts of data and data is stored redundantly to ensure high availability."
+    },
+    new() {
+            Key = 3,
+            Name = "Microsoft Entra ID",
+            Description = "Manage user identities and control access to your apps, data, and resources."
+    },
+    new() {
+            Key = 4,
+            Name = "Azure Key Vault",
+            Description = "Store and access application secrets like connection strings and API keys in an encrypted vault with restricted access to make sure your secrets and your application aren't compromised."
+    },
+    new() {
+            Key = 5,
+            Name = "Azure AI Search",
+            Description = "Information retrieval at scale for traditional and conversational search applications, with security and options for AI enrichment and vectorization."
+    }
+];
